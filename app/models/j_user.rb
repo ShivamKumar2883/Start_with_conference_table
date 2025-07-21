ff054759d7a73db22c504056e571e74a76566a3d
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-21 12:53:20 +0530

    Implemented one-to-one profile association with proper validations

--- app/models/j_user.rb
+++ app/models/j_user.rb
@@ -1,5 +1,5 @@
 class JUser < ApplicationRecord
     has_one :profile, dependent: :destroy
-    validates :email, format: { with: /@/, message: "must contain @ symbol"}
+    validates :email, uniqueness: { case_sensitive: false, message: "is already taken" }, format: { with: /@/, message: "must contain @ symbol"}
     validates :password, length: { minimum: 6 , too_short: "must be at least 6 characters"} 
 end

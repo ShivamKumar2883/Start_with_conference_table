b8a8430fe52bf3e1664b7bf3849593d2f3e6d705
Shivam Kumar <shivamkumar29833@gmail.com>
2025-07-29 12:42:41 +0530

    Your commit message

--- db/migrate/20250728045533_posts.rb
+++ db/migrate/20250728045533_posts.rb
@@ -1,12 +1,14 @@
 class CreatePosts < ActiveRecord::Migration[8.0]
   def change
     create_table :posts do |t|
-      t.string :titile, null:false, foreign_key: true
+      t.string :title, null:false
       t.string :content, null: false
-      t.string :profile_id, null:false
+      t.string :user_id, null:false
       t.string :posted_by, null:false
 
       t.timestamps
   end
+  add_foreign_key :posts, :j_users, column: :user_id
+    add_index :posts, :user_id
 end
 end
